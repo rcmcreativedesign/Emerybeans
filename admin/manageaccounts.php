6552572ecb4d6f0c25e9818df82df5cd049d9f74
Robert McDonnell <rcmcd@hotmail.com>
2023-03-03 21:25:01 -0700

    Manage Invites

--- admin/manageaccounts.php
+++ admin/manageaccounts.php
@@ -1,3 +1,62 @@
 <?php
 require_once '../_authorized.php';
+
+require_once "../classes/Database.php";
+require_once "../classes/User.php";
+
+$database = new Database();
+$db = $database->getConnection();
+$user = new User($db);
+$user->setUserById($_SESSION["id"]);
+
+if (!$user->inviteAuthorized) {
+    echo "Admin only area";
+    exit;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+}
+
+$db->close();
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Emery Beans</title>
+
+        <link href="site.css" rel="stylesheet" type="text/css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anontmous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        
+    </head>
+    <body>
+        <div class="wrapper container">
+            <?php include '../navbar.php'; ?>
+            <h2>Welcome to Emery Beans!</h2>
+            <p>Manage Accounts</p>
+
+            <form id="inviteform" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="row form-group">
+                    <div class="col-sm-2">Display Name</div>
+                    <div class="col-sm-2">Email Address</div>
+                    <div class="col-sm-2">Invite Timestamp</div>
+                    <div class="col-sm-3"></div>
+                </div>
+
+                Edit/Udate
+                Delete
+                Edit
+            </form>
+        </div>
+        <script type="text/javascript">
+            $(function () {
+
+            });
+        </script>
+    </body>
+</html>
---
+++ classes/Admin.php
@@ -0,0 +1,37 @@
+<?php
+class Admin {
+    private $conn;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function getOpenInvites() {
+        $query = "SELECT id, emailAddress, displayName, createdTimestamp FROM user WHERE lastLoginTimestamp IS NULL;";
+        $results = array();
+        if ($stmt = $this->conn->prepare($query)) {
+            if ($stmt->execute()) {
+                $stmtresult = $stmt->get_result();
+
+                if ($stmtresult->num_rows > 0) {
+                    while ($stmtresult_val = $stmtresult->fetch_assoc()) {
+                        $results[] = array($stmtresult_val["id"], $stmtresult_val["emailAddress"], $stmtresult_val["displayName"], $stmtresult_val["createdTimestamp"]);
+                    }
+                }
+            }
+            $stmt->close();
+        }
+        return $results;
+    }
+
+    // public function deleteAccount($id) {
+    //     $query = "DELETE FROM user WHERE id = ?";
+    // }
+
+    // public function deleteEntry($id) {
+    //     $entrylikeQuery = "DELETE FROM entrylike WHERE entryId = ?;";
+    //     $entryviewQuery = "DELETE FROM entryview WHERE entryId = ?";
+    //     $entryQuery = "DELETE FROM entry WHERE id = ?";
+    // }
+}
+?>
