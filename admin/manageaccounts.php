c5c3592cec19750a5ed9b90a0d8f3855c5ca9e9e
Robert McDonnell <rcmcd@hotmail.com>
2023-03-03 22:34:53 -0700

    Fixed admin style sheets reference

--- admin/manageaccounts.php
+++ admin/manageaccounts.php
@@ -27,7 +27,7 @@ $db->close();
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Emery Beans</title>
 
-        <link href="site.css" rel="stylesheet" type="text/css"/>
+        <link href="../site.css" rel="stylesheet" type="text/css"/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anontmous"></script>
@@ -67,7 +67,7 @@ $db->close();
                 $(".edit-link").click(function() {
                     var that = this;
                     $(that).val("Save");
-                    $(that).parent().find("[data-type='displayName']").innerHtml("<input type='text' value='' />");
+                    $(that).parent().find("[data-type='displayName']").empty().append("<input type='text' id='displayName' value='' />");
                 });
                 
             });
--- admin/statistics.php
+++ admin/statistics.php
@@ -33,7 +33,7 @@ $newTodayEntry = 0;
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Emery Beans</title>
 
-        <link href="site.css" rel="stylesheet" type="text/css"/>
+        <link href="../site.css" rel="stylesheet" type="text/css"/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anontmous"></script>
